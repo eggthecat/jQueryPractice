2e1a0e70fbd71039a1563b715cd136894046a61b
dominic montelongo and nikhil batta <dominic@>
2019-08-12 11:35:35 -0700

    Add initial commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+jQuery("h1").click(function(){
+  alert("This is a header.");
+});
+jQuery("p").click(function(){
+  alert("This is a pargraph.");
+})
+jQuery("img").click(function(){
+  alert("This is an image.");
+});
